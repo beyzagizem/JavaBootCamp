31ed17282352a3aeb95cd653277eb98143e53130
beyzagizem <79171658+beyzagizem@users.noreply.github.com>
2022-10-13 11:52:56 +0300

    IsPerfectNumber

---
+++ src/IsPerfectNumber.java
@@ -0,0 +1,20 @@
+public class IsPerfectNumber {
+
+    public static void main (String[] args){
+
+        int numberToCheck =7;
+        int total=0;
+        for(int i=1;i<numberToCheck;i++){
+            if ((numberToCheck % i)==0){
+                total+=i;
+            }
+
+        }
+       String message= (total==numberToCheck) ? "yes perfect number":"no";
+        System.out.println(message);
+
+
+
+
+    }
+}
--- src/IsPrimeNumber.java
+++ src/IsPrimeNumber.java
@@ -1,4 +1,4 @@
-public class PrimeNumbers {
+public class IsPrimeNumber {
 
     public static void main(String[] args) {
 
