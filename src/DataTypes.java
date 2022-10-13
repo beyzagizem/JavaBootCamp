58a7224f21edf539ad311f9d0071b4243d3e09eb
beyzagizem <79171658+beyzagizem@users.noreply.github.com>
2022-10-13 19:54:47 +0300

    other lessons added

---
+++ src/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        double sayi = 1.5;
+        sayi = 129;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+    }
+}
